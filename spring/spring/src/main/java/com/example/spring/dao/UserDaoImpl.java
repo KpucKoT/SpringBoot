fceeee3b641c8d55fc91c1978a33ef1d7e4beb21
KpucKoT <167256393+KpucKoT@users.noreply.github.com>
2024-08-05 23:15:42 +0300

    Add files via upload

---
+++ spring/spring/src/main/java/com/example/spring/dao/UserDaoImpl.java
@@ -0,0 +1,46 @@
+package com.example.spring.dao;
+
+import com.example.spring.model.User;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+public class UserDaoImpl implements UserDao{
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+
+    public List<User> getUsers() {
+        return entityManager.createQuery("from User", User.class).getResultList();
+    }
+
+
+    public void createUser(User user) {
+        entityManager.persist(user);
+    }
+
+
+    public void updateUser(User user) {
+        entityManager.merge(user);
+    }
+
+
+    public void deleteUser(User user) {
+        //entityManager.remove(entityManager.contains(user) ? user : entityManager.merge(user));
+        entityManager.remove(user);
+    }
+
+
+    public User getUser(int id) {
+        return entityManager.find(User.class, id);
+    }
+
+
+}
